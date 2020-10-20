bc16a85d5209a5ca03d79a201a7afb4c91ee1d4f
subbus <subbareddyg83@gmail.com>
2020-10-20 10:15:23 +0530

    attendence check is added

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-echo "Welcome to Employee Wage Computation Program"
+attendence=$(( RANDOM % 2 ))
+if(( attendence == 0 ))
+then
+    echo "Employee is absent"
+else
+    echo "Employee is present"
+fi
